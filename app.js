15d96bc6f6df11c101e351f6958bc2f98a5dcfb9
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-06-21 17:51:06 -0300

    Primera parte: funciones prompt, alert, variables y condicional if

---
+++ app.js
@@ -0,0 +1,29 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor: ");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el número');
+}
+
+
+/* Ejercitacion
+alert('¡Bienvenida y bienvenido a nuestro sitio web!"');
+
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas= 50;
+let saldoDisponible= 1000;
+alert('¡Error! Completa todos los campos');
+let mensajeDeError = '¡Error! Completa todos los campos';
+alert(mensajeDeError);
+
+nombre = prompt('Ingresa tu nombre de usuario: ');
+edad = prompt('Ingresa tu edad: ');
+
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
+
+ */

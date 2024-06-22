2c6022a06a6924968aaaddd93cd9980064f5d298
Mario Cristian Sanchez <mariocrsanchez@gmail.com>
2024-06-21 23:37:51 -0300

    Cuarta parte: Buenas practicas en programación: Break

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ while (numeroUsuario != numeroSecreto) {
         //Incrementamos el contador cuando no acierta
         intentos = intentos + 1;
        palabraVeces = 'veces';
+       if (intentos > 3) {
+        alert('LLegaste al número maximo de intentos');
+        break
+       }
     }
 }
 
@@ -118,7 +122,7 @@ Desafios
 2-Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
 3-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
 4-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
-*/
+
 
 // Ejercicio 1
 contador = 1;
@@ -152,3 +156,4 @@ while(cuentaRegresiva >= 0){
     alert(`Numero ${cuentaRegresiva}`)
     cuentaRegresiva--;
 }
+*/
